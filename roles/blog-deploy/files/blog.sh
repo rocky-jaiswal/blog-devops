f7ae687e8aabeb5d5fe2ce66876870a330dea37a
Rocky Jaiswal <rockyj@rockyj-home-main.fritz.box>
2018-02-17 18:50:24 +0100

    Fix the deploy script.

--- roles/blog-deploy/files/blog.sh
+++ roles/blog-deploy/files/blog.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-cd /home/app/my_blogs
+cd /home/app/my_blog
 git pull origin master
+cd /home/app/my_blogs/current
 docker build -t rockyj/blogs --file=./Dockerfile  .
 docker ps -aq | xargs docker stop | xargs docker rm -fv
 docker run -itd -p 8080:8080 rockyj/blogs
+
